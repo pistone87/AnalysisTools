2ecd41f92cca8bbc4cb08ed4cddf642428740bc0
Claudia Pistone <pistone@physik.rwth-aachen.de>
2014-08-21 15:25:54 +0200

    DrawMacro.C updated: added legend, axes titles, styling git add Makefile (file to compile DrawMacro.C)

--- Code/pistone/DrawMacro.C
+++ Code/pistone/DrawMacro.C
@@ -2,15 +2,19 @@
 #include <stdexcept>   // throw exception
 #include <stdio.h>
 #include <string>   //string, tostring
+#include <sys/stat.h>   //mkdir
+#include <sys/types.h>   //mkdir
 
 #include <TCanvas.h>
 #include <TFile.h>
 #include <TH1.h>
 #include <TLegend.h>
 #include <THStack.h>
+#include <TStyle.h>
 
 using namespace std;
 
+// NOTE: all the names of new objects are in camelCase
 
 // ***** classes & structures & typedef *****
 
@@ -87,9 +91,31 @@ private:
 
 public:
 
-    // constructo
+    // constructor
     varToPlot() : vector<string>(list, list + sizeof(list) / sizeof(char*)) {}
  
+};
+
+
+/// class to have my own legend
+class myLegend : public TLegend{
+
+private:
+
+public:
+    
+    // constructor
+    myLegend(double width=.65, double height=.18) : TLegend(.9-width, .88-height, .9, .88){
+        this->SetBorderSize(0);
+        this->SetFillStyle(4000);
+        this->SetFillColor(0);
+        this->SetTextSize(0.03);
+        this->SetMargin(0.15);
+        this->SetNColumns(3);
+        this->SetColumnSeparation(0.05);
+    }
+
+
 };
 
 
@@ -146,23 +172,24 @@ void drawPlots(){
 
 int main(){
 
+    // create a directory img where to save plots
+    mkdir("img", 0755);
+        
+
     // constants
     string prefix = "lightchargedhiggs_default_";
     
     
     // variables declaration
     allPlotAtt alldatasets;
+    string titleX, titleY;
 
     varToPlot mylist;
 
-    // histograms declaration
-    THStack hs_bkg;
-    TH1D* h_data;
-    TH1D* h_sig;
 
-    // canvas
-    TCanvas *c = new TCanvas("c", "c", 1);
-    
+    // objects to draw plots
+    TCanvas *c = new TCanvas("c", "c", 1);//200, 10, 750, 750);
+    myLegend leg;
 
     // Open the root file with histograms
     TFile *rootfile;
@@ -173,7 +200,11 @@ int main(){
     }
 
 
-    for(unsigned int i=0; i<1; ++i){
+    for(unsigned int i=0; i<mylist.size(); ++i){
+        
+        THStack hs_bkg;
+        TH1D* h_data;
+        TH1D* h_sig;
 
         string vartmp = mylist.at(i);
 
@@ -193,38 +224,78 @@ int main(){
                 throw runtime_error("missing histogram");
             }       
 
+            if(j==0){
+                titleX = htmp->GetXaxis()->GetTitle();
+                titleY = htmp->GetYaxis()->GetTitle();
+            }
+
             if(nametmp == "Data"){
                 cout << "set: "<< nametmp << " doing Data" <<endl;
                 h_data = (TH1D*) htmp->Clone();
                 h_data->SetMarkerColor(colortmp);
+                if(i==0) leg.AddEntry(h_data, (labeltmp).c_str(), "pe");
             } // if data
-
+            
             else if(nametmp == "HplusBWB_M80"){
                 cout << "set: "<< nametmp << " doing H+" <<endl;
                 h_sig = (TH1D*) htmp->Clone();
                 h_sig->SetLineColor(1);
                 h_sig->SetFillColor(colortmp);
+                if(i==0) leg.AddEntry(h_sig, (labeltmp).c_str(), "F");
             } // if signal
 
             else{
                 cout << "set: "<< nametmp << " doing bkg" <<endl;
                 htmp->SetLineColor(1);
                 htmp->SetFillColor(colortmp);
+                if(nametmp=="MC_WJ" || nametmp=="MC_WJ_tau") continue; 
                 hs_bkg.Add(htmp);
+                if(i==0) leg.AddEntry(htmp, (labeltmp).c_str(), "F");
             } // if bkg
 
  
         } // loop over all the datasets
 
         c->cd();
+
+        c->SetLogy(0);
         
-        //gPad->SetMargin(0.15,0.1,0.15,0.1);
-        
+        gPad->SetMargin(0.15,0.1,0.15,0.1);
+        gPad->SetTicks(1, 1);       
+ 
+        double max = h_data->GetBinContent(h_data->GetMaximumBin());
+
+        hs_bkg.SetMinimum(0);
+        hs_bkg.SetMaximum(1.5*max);
         hs_bkg.Draw("hist");
-        h_sig->Draw("histsame");
+
+        hs_bkg.GetXaxis()->SetTitle((titleX).c_str());
+        hs_bkg.GetXaxis()->SetTitleOffset(1.2);
+        hs_bkg.GetXaxis()->SetTitleSize(0.05);
+        hs_bkg.GetXaxis()->SetLabelSize(0.05);
+    
+        hs_bkg.GetYaxis()->SetTitle((titleY).c_str());
+        hs_bkg.GetYaxis()->SetTitleOffset(1.3);
+        hs_bkg.GetYaxis()->SetTitleSize(0.05);
+        hs_bkg.GetYaxis()->SetLabelSize(0.05);
+        
+
+        //h_sig->Draw("histsame");
         h_data->Draw("EPsame");
+        h_data->Draw("axissame");
+        leg.Draw("same");
         
         c->SaveAs(("./img/"+vartmp+".eps").c_str());
+
+
+        c->SetLogy(1);
+        
+        hs_bkg.SetMinimum(0.01);
+        hs_bkg.SetMaximum(100*max);
+
+        c->SaveAs(("./img/"+vartmp+"log.eps").c_str());
+
+        c->Clear();
 
     } // loop over mylist
 
